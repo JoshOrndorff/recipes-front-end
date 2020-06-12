727e7e31dec9552c1ab410d8c9754458245b78d5
Joshy Orndorff <admin@joshyorndorff.com>
2020-06-11 22:05:01 -0400

    Render recipes in layout

--- src/templates/recipe-template.js
+++ src/templates/recipe-template.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Layout from "../components/layout"
 
 export default function RecipeTemplate({
   data, // this prop will be injected by the GraphQL query below.
@@ -7,20 +8,22 @@ export default function RecipeTemplate({
   const { markdownRemark } = data // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
   return (
-    <div className="recipe-container">
-      <div className="recipe">
-        <h1>{frontmatter.title}</h1>
-				<a
-          href={frontmatter.code}
-        >
-					View Code on GitHub
-				</a>
-        <div
-          className="recipe-content"
-          dangerouslySetInnerHTML={{ __html: html }}
-        />
-      </div>
-    </div>
+		<Layout>
+	    <div className="recipe-container">
+	      <div className="recipe">
+	        <h1>{frontmatter.title}</h1>
+					<a
+	          href={frontmatter.code}
+	        >
+						View Code on GitHub
+					</a>
+	        <div
+	          className="recipe-content"
+	          dangerouslySetInnerHTML={{ __html: html }}
+	        />
+	      </div>
+	    </div>
+		</Layout>
   )
 }
 
